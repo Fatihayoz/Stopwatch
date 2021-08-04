7a406c934c7224c524bd27d570b7e43c8c279b40
fatih <fatihayoz20800@gmail.com>
2021-08-05 00:54:32 +0200

    prettier

--- init.js
+++
@@ -1,3 +0,0 @@
-import "./js/listeners/startButton.js";
-import "./js/listeners/lapsButton.js";
-import "./js/listeners/resetButton.js";
---
+++ js/init.js
@@ -0,0 +1,3 @@
+import "./listeners/startButton.js";
+import "./listeners/lapsButton.js";
+import "./listeners/resetButton.js";
